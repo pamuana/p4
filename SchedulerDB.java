6ddd3d37e1fa128aaf8656a57fb99b9e46089a62
pamuana <abhattachar4@wisc.edu>
2015-11-29 16:34:47 -0600

    Update SchedulerDB.java

--- SchedulerDB.java
+++ SchedulerDB.java
@@ -5,65 +5,120 @@ import java.util.*;
 
 
 public class SchedulerDB {
-    IntervalBST<Resource> resources;
+    List<Resource> resources;
 
     SchedulerDB(){
-        resources = = new IntervalBST<Resource>();
+        resources = new ArrayList<Resource>();
     }
 
     public boolean addResource(String resource){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("addResource() not implemented.");
+        return resources.add(new Resource(resource));
     }
 
     public boolean removeResource(String r){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("removeResource() not implemented.");
+        Resource re = findResource(r);
+        return resources.remove(re);
     }
 
     public boolean addEvent(String r, long start, long end, String name,
                             String organization, String description){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("addEvent() not implemented.");
+        Resource re = findResource(r);
+        return re.addEvent(new Event(start, end, name, r, organization, description));
     }
 
     public boolean deleteEvent(long start, String resource){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("deleteEvent() not implemented.");
+    	Resource re = findResource(resource);
+    	return re.deleteEvent(start);
     }
 
     public Resource findResource(String r){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("findResource() not implemented.");
+        Iterator<Resource> itr = resources.iterator();
+        Resource res = null;
+        System.out.println("Find Res: Compare to " + r);
+        while (itr.hasNext()){
+        	Resource temp = itr.next();
+        	System.out.println("Itr " + temp.getName());
+        	if (temp.getName().equals(r)){
+        		res = temp;
+        	}
+        }
+        return res;
     }
 
     public List<Resource> getResources(){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getResources() not implemented.");
+        return resources;
     }
 
     public List<Event> getEventsInResource(String resource){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getEventsInReource() not implemented.");
+        Iterator<Event> itr = findResource(resource).iterator();
+        List<Event> events = new ArrayList<Event>();
+        while (itr.hasNext()){
+        	events.add(itr.next());
+        }
+        return events;
     }
 
     public List<Event> getEventsInRange(long start, long end){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getEventsInRange() not implemented.");
+        List<Event> events = new ArrayList<Event>();
+        Iterator<Resource> itrRe = resources.iterator();
+        while (itrRe.hasNext()){
+        	Resource tempRe = itrRe.next();
+        	Iterator<Event> itrEv = tempRe.iterator();
+        	while(itrEv.hasNext()){
+        		Event tempEv = itrEv.next();
+        		Event test = new Event(start, end, "test","test","test","test");
+        		if (tempEv.overlap(test)){
+        			events.add(tempEv);
+        		}
+        		
+        	}
+        }
+        return events;
     }
 
     public List<Event> getEventsInRangeInResource(long start, long end, String resource){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getEventsInRangeInReource() not implemented.");
+    	 List<Event> events = new ArrayList<Event>();
+         Resource tempRe = findResource(resource);
+         Iterator<Event> itrEv = tempRe.iterator();
+         while(itrEv.hasNext()){
+        	Event tempEv = itrEv.next();
+        	Event test = new Event(start, end, "test","test","test","test");
+    		if (tempEv.overlap(test)){
+    			events.add(tempEv);
+    		}
+         	}
+         return events;
     }
+    
+    
 
     public List<Event> getAllEvents(){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getAllEvents() not implemented.");
+    	List<Event> events = new ArrayList<Event>();
+        Iterator<Resource> itrRe = resources.iterator();
+        while (itrRe.hasNext()){
+        	Resource tempRe = itrRe.next();
+        	Iterator<Event> itrEv = tempRe.iterator();
+        	while(itrEv.hasNext()){
+        		Event tempEv = itrEv.next();
+        		events.add(tempEv);
+        	}
+        }
+        return events;
     }
 
     public List<Event> getEventsForOrg(String org){
-        //TODO Remove this exception and implement the method
-        throw new RuntimeException("getEventsForOrg() not implemented.");
+    	List<Event> events = new ArrayList<Event>();
+        Iterator<Resource> itrRe = resources.iterator();
+        while (itrRe.hasNext()){
+        	Resource tempRe = itrRe.next();
+        	Iterator<Event> itrEv = tempRe.iterator();
+        	while(itrEv.hasNext()){
+        		Event tempEv = itrEv.next();
+        		if (tempEv.getOrganization().equals(org)){
+        			events.add(tempEv);
+        		}
+        	}
+        }
+        return events;
     }
 }
